ab63732175d662cd49bbeafd677d21363ba5e7e5
Alex Radulescu <allex.radulescu91@gmail.com>
2023-04-24 11:31:10 +0200

    fix: add await

--- test/specs/meditations-actions.spec.ts
+++ test/specs/meditations-actions.spec.ts
@@ -4,8 +4,8 @@ describe('meditation session selection', () => {
   it('should favourite and unfavourite meditation', async () => {
     // Using Accessibility IDs as exemplified here:
     // https://webdriver.io/docs/selectors/#accessibility-id
-    homeScreen.favouriteMeditation()
-    homeScreen.unfavouriteMeditation()
+    await homeScreen.favouriteMeditation()
+    await homeScreen.unfavouriteMeditation()
 
     const favouriteList = await homeScreen.scrollToElement('favourites_list')
     await favouriteList.waitForExist({ timeout: 5000, reverse: true })
