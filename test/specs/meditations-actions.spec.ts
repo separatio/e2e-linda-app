b9b78abec81fee81e51b2ae92e3dfdfb6ca71fe0
Alex Radulescu <allex.radulescu91@gmail.com>
2023-04-24 11:15:17 +0200

    feat: apply page object model

--- test/specs/meditations-actions.spec.ts
+++ test/specs/meditations-actions.spec.ts
@@ -1,23 +1,13 @@
+import homeScreen from "../support/screenobjects/home.screen"
+
 describe('meditation session selection', () => {
-  it.skip('should favourite and unfavourite meditation', async () => {
+  it('should favourite and unfavourite meditation', async () => {
     // Using Accessibility IDs as exemplified here:
     // https://webdriver.io/docs/selectors/#accessibility-id
-    const firstMeditation = await $('~meditation0')
-    await firstMeditation.click()
+    homeScreen.favouriteMeditation()
+    homeScreen.unfavouriteMeditation()
 
-    const favouriteButton = await $('~favourite_button')
-    await favouriteButton.click()
-
-    const backButton = await $('~Go back')
-    await backButton.click()
-
-    const favouriteSectionSelector = `new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().description("favourites_list"))`
-    const favouriteList = await $(`android=${favouriteSectionSelector}`)
-
-    await favouriteList.$('~meditation0').click()
-    await favouriteButton.click()
-    await backButton.click()
-
+    const favouriteList = await homeScreen.scrollToElement('favourites_list')
     await favouriteList.waitForExist({ timeout: 5000, reverse: true })
     expect(await favouriteList.isExisting()).toBe(false);
   })
