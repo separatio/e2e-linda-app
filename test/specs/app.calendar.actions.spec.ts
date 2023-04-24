7dfddfe4b984fb70a2021f84e4848b286f26240f
Alex Radulescu <allex.radulescu91@gmail.com>
2023-04-24 10:56:43 +0200

    feat: finish first test

--- test/specs/app.calendar.actions.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('when requesting to erase all data', () => {
-  it.skip('should erase all user data', async () => {})
-
-  it.skip('should display random quote', async () => {})
-})
--- test/specs/app.player.actions.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('player actions', () => {
-  it.skip('should play the sound', async () => {})
-
-  it.skip('should pause the sound', async () => {})
-
-  it.skip('should seek forward and backward', async () => {})
-})
